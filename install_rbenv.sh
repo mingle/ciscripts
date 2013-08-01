8902075f95546534f098ecfb4f4fb44f340eb79e
tworker <tworker@thoughtworks.com>
2013-08-01 11:18:32 -0700

    add install rbenv script

---
+++ install_rbenv.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -x
+set -e
+
+
+# Install rbenv from scratch on linux build agents
+# usage:
+#  export RBENV_VERSION=ruby-1.9.3
+#  sh install_rbenv.sh
+
+if [ ! -d ~/.rbenv ]; then
+    echo "installing rbenv..."
+    git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+fi
+
+if [ ! -d ~/.rbenv/plugins/ruby-build ]; then
+    echo "installing ruby-build plugin..."
+    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
+
+if [ ! -d ~/.rbenv/versions/$RBENV_VERSION ]; then
+    echo "installing ruby ${RBENV_VERSION}..."
+    ~/.rbenv/bin/rbenv install $RBENV_VERSION
+    echo "done"
+fi
+
+echo "installing gems:"
+if [[ $RBENV_VERSION == jruby* ]]
+then
+    ~/.rbenv/bin/rbenv exec gem install jruby-openssl
+fi
+
+~/.rbenv/bin/rbenv exec gem install bundler
+~/.rbenv/bin/rbenv exec ruby -S bundle install
+~/.rbenv/bin/rbenv rehash
